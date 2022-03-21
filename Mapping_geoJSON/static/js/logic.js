8c36c514e770fbd938e1bae4a3ef13527f83c1de
Jonathan Tree <46392976+jonathantree@users.noreply.github.com>
2022-03-20 20:24:28 -0700

    Adding Airport GeoJSON

---
+++ Mapping_geoJSON/static/js/logic.js
@@ -0,0 +1,43 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
+
+
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+var darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    accessToken: API_KEY
+  });
+
+var satmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/satellite-streets-v11',
+  accessToken: API_KEY
+});
+
+
+
+// Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Street Map": streets,
+    "Dark Map": darkmap
+  };
+
+// Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+satmap.addTo(map);
+
+
